11a3e9a69ae7a1a359c4a06eb0e144db43b04cda
Masaiha <masanori.iha@gmail.com>
2020-08-12 15:52:10 -0300

    Criando Estrutural inicial de projetos

---
+++ GerenciadorDeTarefas/src/MasaIO.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MasaIO.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ GerenciadorDeTarefas/src/MasaIO.business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MasaIO.business
+{
+    public class Class1
+    {
+    }
+}
